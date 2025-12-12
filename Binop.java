bc2443f2272bb5941785ec4498eb781c35657440
willhuff99 <whuff272@wcupa.edu>
2025-12-11 19:54:04 -0500

    Create Binop.java

---
+++ Binop.java
@@ -0,0 +1,7 @@
+/**
+ * Code Template Author: David G. Cooper
+ * Purpose: A base class for binary arithmetic operations
+ */
+public abstract class Binop extends Op {
+     public abstract double eval(double left, double right);
+}
